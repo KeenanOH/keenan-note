97b5bb6b1758143e198fd7775c33671698e875b6
Keenan <86394469+KeenanOH@users.noreply.github.com>
2024-01-21 21:00:49 -0600

    Update Dashboard.tsx

--- src/app/dashboard/_components/Dashboard.tsx
+++ src/app/dashboard/_components/Dashboard.tsx
@@ -6,7 +6,7 @@ import NoteEditor from "@/app/dashboard/_components/NoteEditor"
 export default function Dashboard({ noteId }: { noteId?: string }) {
 
     return (
-        <div className="flex flex-col md:flex-row items-center md:items-start p-8 gap-x-8 h-screen">
+        <div className="flex flex-col md:flex-row items-center md:items-start p-8 gap-x-8 gap-y-8 h-screen">
             <Sidebar />
             { noteId &&
                 <NoteEditor noteId={ noteId } />
