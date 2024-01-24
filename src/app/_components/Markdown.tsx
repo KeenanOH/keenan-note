d970b6ba5ae94058b139ecdae0e3f70cd788e008
KeenanOH <86394469+KeenanOH@users.noreply.github.com>
2024-01-23 21:22:34 -0600

    Added remark-breaks

--- src/app/_components/Markdown.tsx
+++ src/app/_components/Markdown.tsx
@@ -6,12 +6,13 @@ import remarkMath from "remark-math"
 import "katex/dist/katex.min.css"
 import {Prism as SyntaxHighlighter} from "react-syntax-highlighter"
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism"
+import remarkBreaks from "remark-breaks"
 
 export default function Markdown({ children }: { children: string }) {
     return (
         <RMarkdown
             className="prose p-8"
-            remarkPlugins={ [remarkGfm, remarkMath] }
+            remarkPlugins={ [remarkGfm, remarkMath, remarkBreaks] }
             rehypePlugins={ [rehypeKatex] }
             components={ {
                 code: ({ children, className }) => {
